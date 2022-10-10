19025495349344348640d94ca661ef19892a04db
2xente <113365618+2xente@users.noreply.github.com>
2022-10-10 15:32:32 +0200

    Delete formulairephp directory

--- formulairephp/thanks.php
+++
@@ -1,9 +0,0 @@
-<?php 
-   var_dump($_POST);
-
-echo   "Merci".$_POST['user_prenom']." ".$_POST['user_nom']."de nous avoir contacté à propos de ".$_POST['subject'].
-
-"Un de nos conseillers vous contactera soit à l’adresse ".$_POST['user_email']." ou par téléphone au ".$_POST['user_phone']."  dans les plus brefs délais pour traiter votre demande : ";
-
-echo $_POST['user_message'];
-    ?>
